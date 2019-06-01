053474fc966a871e62a3c19121260a619a667763
Leon Linton <leon.linton@live.co.uk>
2019-06-01 11:46:02 +0100

    Added test and code for 'no bikes available'

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,23 +1,33 @@
 require 'docking_station'
-describe DockingStation do
-#    it responds to release_bike do
-#     expect(subject).to respond_to :release_bike
-#    end
-it { is_expected.to respond_to :release_bike }
-
-it 'releases working bikes' do
-    bike = subject.release_bike
-    expect(bike).to be_working
+  describe DockingStation do
+  #    it responds to release_bike do
+  #     expect(subject).to respond_to :release_bike
+  #    end
+  it { is_expected.to respond_to :release_bike }
+=begin
+  it 'releases working bikes' do
+      bike = subject.release_bike
+      expect(bike).to be_working
   end
+=end
+    describe '#release_bike' do
+      it 'raises an error when there are no bikes available' do
+        expect { subject.release_bike }.to raise_error 'No bikes available'
+      end
+      it 'releases a bike' do
+        bike = Bike.new
+        subject.dock(bike)
+        expect(subject.release_bike). to eq bike
+        end
+      end
+  it { is_expected.to respond_to(:dock).with(1).argument }
 
-it { is_expected.to respond_to(:dock).with(1).argument }
+  it { is_expected.to respond_to(:bike)}
 
-it { is_expected.to respond_to(:bike)}
-
-it 'returns docked bikes' do
- bike = Bike.new
- subject.dock(bike)
- #to return the we bike we dock
- expect(subject.bike).to eq bike
-end
+  it 'returns docked bikes' do
+   bike = Bike.new
+   subject.dock(bike)
+   #to return the we bike we dock
+   expect(subject.bike).to eq bike
+  end
 end
